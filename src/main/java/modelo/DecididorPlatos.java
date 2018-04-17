f1628dd67b2e9004b069c0a5c05f88fb61b3c20b
Matias <Matias@192.168.0.12>
2018-04-17 13:14:34 -0300

    Arreglo de dataColector en decididorPlatos

--- src/main/java/modelo/DecididorPlatos.java
+++ src/main/java/modelo/DecididorPlatos.java
@@ -5,24 +5,24 @@ import java.util.HashMap;
 
 public class DecididorPlatos {
 	
-	public HashMap<Ingrediente, Cantidad> cantidadIngredientes (Plato plato){
+	public HashMap<Ingrediente, Cantidad> cantidadIngredientes (Plato plato,DataColector Data){
 		
 		HashMap<Ingrediente, Cantidad> ret = new HashMap<Ingrediente, Cantidad>();
 		ArrayList<Ingrediente> ingredientes = AnalizadorPlatos.dameIngredientes(plato);
-		DataColector dataColector = new DataColector();
+		DataColector dataColector=Data;
 		for(Ingrediente i: ingredientes) {
 			ret.put(i, dataColector.getCantidad(i));
 		}
 		return ret;
 	}
 	
-	public boolean esPosibleArmar (Plato plato) {
+	public boolean esPosibleArmar (Plato plato,DataColector dataColector) {
 		
 		boolean ret = true;
 		
 		ArrayList<Ingrediente> ingredientes = AnalizadorPlatos.dameIngredientes(plato);
 		HashMap<Ingrediente, Cantidad> receta = AnalizadorPlatos.dameIngredientesyCantidad(plato);
-		HashMap<Ingrediente, Cantidad> stock = cantidadIngredientes(plato);
+		HashMap<Ingrediente, Cantidad> stock = cantidadIngredientes(plato,dataColector);
 		
 		for(Ingrediente i: ingredientes) {
 			
