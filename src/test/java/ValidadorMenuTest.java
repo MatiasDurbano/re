69b59bd9bfce2a56db3a35ad7021b26f14640d58
Kei <Kei@DESKTOP-S6SV6GG>
2018-04-17 03:08:18 -0300

    fix test menu

--- src/test/java/ValidadorMenuTest.java
+++ src/test/java/ValidadorMenuTest.java
@@ -3,13 +3,18 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
 
 import Validadores.ValidadorMenu;
+import modelo.Cantidad;
+import modelo.Ingrediente;
+import modelo.Medicion;
 import modelo.Menu;
 import modelo.Plato;
+import modelo.Receta;
 
 public class ValidadorMenuTest {
 
@@ -17,8 +22,36 @@ public class ValidadorMenuTest {
 	public void test() {
 		Menu menu = new Menu();		
 		assertTrue(ValidadorMenu.MenuVacio(menu));
-		assertFalse(menu.getFecha_de_Creacion() == null);		
-		ValidadorMenu.SacaRepetidoYCeros(menu);		
+		
+		//---
+		Ingrediente tomate = new Ingrediente("tomate");
+		Ingrediente fideos = new Ingrediente("fideos");
+		Cantidad diez = new Cantidad(Medicion.Kg, 10);
+		Cantidad ocho = new Cantidad(Medicion.Kg, 8);
+		Map<Ingrediente, Cantidad> am = new HashMap<Ingrediente, Cantidad>();
+		am.put(tomate, diez);
+		am.put(fideos, ocho);
+		Receta ar = new Receta(am);
+		String as = "Fideos con tuco";
+		Plato a = new Plato(as, ar);
+		
+		Ingrediente carne = new Ingrediente("carne");
+		Ingrediente papas = new Ingrediente("papas");
+		Cantidad cinco = new Cantidad(Medicion.Kg, 5);
+		Cantidad cuatro = new Cantidad(Medicion.Unidad, 4);
+		Map<Ingrediente, Cantidad> bm = new HashMap<Ingrediente, Cantidad>();
+		bm.put(carne, cinco);
+		bm.put(papas, cuatro);
+		Receta br = new Receta(am);
+		String bs = "Carne al horno con papas";
+		Plato b = new Plato(bs, br);		
+				
+		//---
+		
+		menu.agregarPlato(a, 1);
+		
+		assertTrue(menu.getFecha_de_Creacion() != null);		
+		menu = ValidadorMenu.SacaRepetidoYCeros(menu);		
 		for(Map.Entry<Plato, Integer> entry: menu.getPlatos().entrySet())
 		{
 			assertTrue(entry.getValue() >= 0);
