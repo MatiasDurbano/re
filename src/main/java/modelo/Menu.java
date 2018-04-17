7f0d6cd06e005c7a50f725a61849937afdbc070e
Kei <Kei@DESKTOP-S6SV6GG>
2018-04-16 22:34:45 -0300

    add - Constructor menu

--- src/main/java/modelo/Menu.java
+++ src/main/java/modelo/Menu.java
@@ -1,8 +1,13 @@
 package modelo;
+import java.util.ArrayList;
 import java.util.List;
-
 
 public class Menu 
 {
-	List<Plato> platos;
+	List<Plato> platos;	
+	
+	public Menu() 
+	{
+		this.platos = new ArrayList<Plato>();
+	}
 }
