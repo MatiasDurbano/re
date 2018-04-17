bbec7f79d18d561d3720ac32c292ad2263003b20
Kei <Kei@DESKTOP-S6SV6GG>
2018-04-17 01:47:02 -0300

    fix test menuvalidador

--- src/test/java/ValidadorMenuTest.java
+++ src/test/java/ValidadorMenuTest.java
@@ -3,10 +3,13 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Map;
+
 import org.junit.Test;
 
 import Validadores.ValidadorMenu;
 import modelo.Menu;
+import modelo.Plato;
 
 public class ValidadorMenuTest {
 
@@ -14,8 +17,14 @@ public class ValidadorMenuTest {
 	public void test() {
 		Menu menu = new Menu();		
 		assertTrue(ValidadorMenu.MenuVacio(menu));
+		assertFalse(menu.getFecha_de_Creacion() == null);
 		
 		
+		
+		for(Map.Entry<Plato, Integer> entry: menu.getPlatos().entrySet())
+		{
+			assertTrue(entry.getValue() >= 0);
+		}
 		
 		
 		
