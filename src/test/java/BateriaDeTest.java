336af8e97adef0084e7b957d0d7842041a1bb8d7
Gpesado <gonzapesa@gmail.com>
2018-04-24 22:40:23 -0300

    BATERIA DE TEST CON METODOS

--- src/test/java/BateriaDeTest.java
+++ src/test/java/BateriaDeTest.java
@@ -1,4 +1,28 @@
 
-public class BateriaDeTest {
-
+public class BateriaDeTest 
+{
+	public static void dameIngredientes()
+	{
+		
+	}
+	public static void damePlatos()
+	{
+		
+	}
+	public static void dameProxyPlato()
+	{
+		
+	}
+	public static void dameProxyIngrediente()
+	{
+		
+	}
+	public static void dameProxyCache()
+	{
+		
+	}
+	public static void dameDataColector()
+	{
+		
+	}
 }
