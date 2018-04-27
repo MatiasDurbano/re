2b2cc792824d0f42a465d522720b32c5b1d721a6
Kei <Kei@DESKTOP-S6SV6GG>
2018-04-27 04:13:43 -0300

    Puntuador

--- src/main/java/modelo/Puntuador.java
+++ src/main/java/modelo/Puntuador.java
@@ -1,5 +1,27 @@
 package modelo;
 
-public class Puntuador {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
+public class Puntuador {
+	
+	public Integer puntuarPlato(Plato plato,StockCantidad stock) {		
+		List<Integer> list = new ArrayList<Integer>();
+		Receta r = plato.getReceta();		
+		for (Map.Entry<Ingrediente, Cantidad> entry : r.getReceta().entrySet()) {
+			double num = this.stockIngrediente(entry.getKey(), stock) / this.cantidadIngredienteReceta(entry.getKey(), plato);
+			list.add((int)num);
+		}		
+		return list.indexOf(Collections.min(list));		
+	}
+	
+	public double cantidadIngredienteReceta(Ingrediente i, Plato p) {
+		return p.getReceta().getReceta().get(i).getValor();
+	}	
+	public double stockIngrediente(Ingrediente i,StockCantidad s) {		
+		return s.getCantidad(i).getValor();
+	}
 }
