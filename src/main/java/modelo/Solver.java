76cedfd149d8191da95c239bdcaab11c588cea4a
Kei <Kei@DESKTOP-S6SV6GG>
2018-04-27 11:24:20 -0300

    Solver

--- src/main/java/modelo/Solver.java
+++ src/main/java/modelo/Solver.java
@@ -1,15 +1,25 @@
 package modelo;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Solver {
-	private Puntuador rank;
+	private Puntuador puntuador;
 	private Map<Plato,Integer> platosYcantidad;
 	
 	public Solver() {
-		Puntuador rank = new Puntuador();	
+		this.puntuador = new Puntuador();	
 		this.platosYcantidad = new HashMap<Plato,Integer>();
+	}
+	public Map<Plato,Integer> getPlatosYcantidad(){
+		return this.platosYcantidad;
+	}
+	public void puntuarPlatos(List<Plato> platos, StockCantidad stock) {
+		for(Plato p : platos) {
+			Integer i=puntuador.puntuarPlato(p, stock);
+			this.platosYcantidad.put(p, i);
+		}
 	}
 	
 }
