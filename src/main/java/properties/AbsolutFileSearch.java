fc1a66cf7f5472348a84aecb6ea23ad6c25dc48f
Kei <Kei@Kei-PC>
2018-05-23 02:27:58 -0300

    Absolute Address properties = Seteable

--- src/main/java/properties/AbsolutFileSearch.java
+++ src/main/java/properties/AbsolutFileSearch.java
@@ -5,12 +5,17 @@ import java.io.FileReader;
 
 public class AbsolutFileSearch implements PropertiesFileSearch {
 	
+	private String ruta = "src/main/java/properties/archivo.properties";
+	
 	public AbsolutFileSearch() {
 	}			
 	
 	public FileReader getFileAddress() throws FileNotFoundException	{
-		FileReader ret = new FileReader("src/main/java/properties/archivo.properties");
+		FileReader ret = new FileReader(ruta);
 		return ret;
 	}	
+	public void setAddress(String nuevaRuta) {
+		this.ruta = nuevaRuta;
+	}
 
 }
