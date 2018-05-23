399d27be99d696d476ce7e82b5207b0a501d6543
Kei <Kei@Kei-PC>
2018-05-23 16:32:00 -0300

    test properties

---
+++ src/test/java/PropertiesLoaderTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import properties.DataConfig;
+import properties.PropertiesLoader;
+public class PropertiesLoaderTest {
+	
+	@Test
+	public void test() {
+		
+		PropertiesLoader pLoader = new PropertiesLoader();
+		DataConfig dConfig;		
+		dConfig = pLoader.getDataConfig();
+		assertTrue(dConfig.getCachesize() == 5);
+		assertFalse(dConfig.getCachesize() == null);
+		assertEquals(dConfig.getFilter(),"celiacos");		
+		
+	}
+
+}
