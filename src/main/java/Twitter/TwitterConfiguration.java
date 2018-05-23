7b080416a39d18ca23e2a6041383b971311beb32
Matias <Matias@192.168.0.16>
2018-05-22 23:26:36 -0300

    final string twitterConfiguration

--- src/main/java/Twitter/TwitterConfiguration.java
+++ src/main/java/Twitter/TwitterConfiguration.java
@@ -13,15 +13,18 @@ public class TwitterConfiguration {
 	private Twitter twitter;
 	private String user;
 
-	
+	final private String ConsumerKey = "PbyHTI9dAwJLY3WZY3ITLgt9i";
+	final private String ConsumerSecret ="euIqLIT3HzHoXfpsCAht8evh7saQGgPytn1Yznx4A3O50jLvVK";
+	final private String OAuthAccessToken ="489600798-0FsdR9M4oT2Imd9uaQWQryzNFESEsOSBa6Rfa0Tw";
+	final private String AccessTokenSecret = "bFs1J80brFYaPsvVv2f6c6lFudlYVuFKfUuNKUrcEGN0Z";
 	
 	public TwitterConfiguration(String arg0) throws TwitterException{
 		config  = new ConfigurationBuilder();
 		config.setDebugEnabled(true)
-		  .setOAuthConsumerKey("PbyHTI9dAwJLY3WZY3ITLgt9i")
-		  .setOAuthConsumerSecret("euIqLIT3HzHoXfpsCAht8evh7saQGgPytn1Yznx4A3O50jLvVK")
-		  .setOAuthAccessToken("489600798-0FsdR9M4oT2Imd9uaQWQryzNFESEsOSBa6Rfa0Tw")
-		  .setOAuthAccessTokenSecret("bFs1J80brFYaPsvVv2f6c6lFudlYVuFKfUuNKUrcEGN0Z");
+		  .setOAuthConsumerKey(ConsumerKey)
+		  .setOAuthConsumerSecret(ConsumerSecret)
+		  .setOAuthAccessToken(OAuthAccessToken)
+		  .setOAuthAccessTokenSecret(AccessTokenSecret);
 		tf = new TwitterFactory(config.build());
 		twitter = tf.getInstance();
 		this.user = arg0;
--- src/test/java/EntryTest.java
+++ src/test/java/EntryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import Twitter.Entry;
 import Twitter.ProxyTwitter;
 import Twitter.ProxyTwitterImp;
+import twitter4j.Status;
 import twitter4j.TwitterException;
 
 public class EntryTest {
@@ -14,11 +15,11 @@ public class EntryTest {
 		ProxyTwitter twitter= new ProxyTwitterImp("elconito94");
 		BateriaDeTest bateria = new BateriaDeTest();
 		Entry entry = new Entry(bateria.damePlatos());
-		 
+		
+		
+		
 		assertEquals(1,entry.verificadorDeFormato(twitter.getTweet()).size());
-		
-		 
-		
+			
 	}
 
 }
