c04e81026d5cb3dfe5d3216b1d535022e92aa763
Gpesado <gonzapesa@gmail.com>
2018-05-23 02:22:45 -0300

    Implementado LocalFileSearch

--- src/main/java/properties/LocalFileSearch.java
+++ src/main/java/properties/LocalFileSearch.java
@@ -6,9 +6,15 @@ import java.io.FileReader;
 public class LocalFileSearch implements PropertiesFileSearch{
 
 	@Override
-	public FileReader getFileAddress() throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public FileReader getFileAddress() throws FileNotFoundException 
+	{
+		//obtengo la ruta donde esta el jar en ejecucion
+		String rutaLocal = this.getClass().getProtectionDomain().getCodeSource().getLocation().toString();
+		rutaLocal = rutaLocal.substring(1); //borro una barra / que me deja al principio
+		
+		FileReader ret = new FileReader(rutaLocal);
+		return ret;
+
 	}
 	
 
