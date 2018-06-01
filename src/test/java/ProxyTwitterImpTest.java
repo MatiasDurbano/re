15c8a19490056376bd3a9c2cb1be777824d83fe7
Matias <Matias@192.168.0.16>
2018-06-01 15:16:17 -0300

    dasdsa

---
+++ src/test/java/ProxyTwitterImpTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Twitter.ProxyTwitter;
+import Twitter.ProxyTwitterImp;
+
+public class ProxyTwitterImpTest {
+	
+	@Test
+	public void test() {
+		ProxyTwitter a = new ProxyTwitterImp();
+		
+		assertEquals(2,2);
+		
+		
+	}
+	
+
+}
---
+++ src/test/java/SearchEngineImpTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Analizador.SearchEngineTweets;
+import Analizador.SearchEngineTweetsImp;
+
+public class SearchEngineImpTest {
+
+	@Test
+	public void test() {
+		SearchEngineTweets a = new SearchEngineTweetsImp();
+		
+		assertEquals(3,3);
+		
+	}
+}
