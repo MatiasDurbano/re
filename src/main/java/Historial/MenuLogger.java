f1807fec77b3726e7c48481587f40ade6bb76602
HeumaX <HeumaX@DESKTOP-R60O13B>
2018-06-01 14:31:26 -0300

    menulooger

--- src/main/java/Historial/MenuLogger.java
+++ src/main/java/Historial/MenuLogger.java
@@ -44,19 +44,6 @@ public class MenuLogger {
 		this.proxyRecord.writeMenues(aActualizar);
 	}
 	
-	public int platosEncontrados(Plato plato) {
-		int ret = 0;
-		Menu[] menus = this.obtenerMenus();
-		
-		for(int i = 0; i < menus.length; i ++) {
-			if(menus[i].getPlatos().contains(plato)) {
-				ret++;
-			}
-		}
-		
-		return ret;
-	}
-	
 	public Menu[] obtenerMenus() {
 		return this.registroMenus.menus;
 	}
