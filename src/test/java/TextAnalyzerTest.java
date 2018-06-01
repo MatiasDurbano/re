90503c47a0227479819f2c93c00554708729ccc4
HeumaX <HeumaX@DESKTOP-R60O13B>
2018-06-01 13:08:25 -0300

    rer

--- src/test/java/TextAnalyzerTest.java
+++ src/test/java/TextAnalyzerTest.java
@@ -8,9 +8,8 @@ public class TextAnalyzerTest {
 	
 	@Test
 	public void test1() {
-		TextAnalyzer textAnalyzer = new TextAnalyzer();
-		Assert.assertFalse(textAnalyzer.encontrar("pure", "buenos dias"));
-		Assert.assertTrue(textAnalyzer.encontrar("pure", "que rico pure"));
-		Assert.assertFalse(textAnalyzer.encontrar("puxr", "que rico pure"));
+		Assert.assertFalse(TextAnalyzer.encontrar("pure", "buenos dias"));
+		Assert.assertTrue(TextAnalyzer.encontrar("pure", "que rico pure"));
+		Assert.assertFalse(TextAnalyzer.encontrar("puxr", "que rico pure"));
 	}
 }
