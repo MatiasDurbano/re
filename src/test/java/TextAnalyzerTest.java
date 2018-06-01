32f868b54ec51a89f0e33351d998647e07194da5
HeumaX <HeumaX@DESKTOP-R60O13B>
2018-06-01 13:09:14 -0300

    erwr

--- src/test/java/TextAnalyzerTest.java
+++ src/test/java/TextAnalyzerTest.java
@@ -11,5 +11,6 @@ public class TextAnalyzerTest {
 		Assert.assertFalse(TextAnalyzer.encontrar("pure", "buenos dias"));
 		Assert.assertTrue(TextAnalyzer.encontrar("pure", "que rico pure"));
 		Assert.assertFalse(TextAnalyzer.encontrar("puxr", "que rico pure"));
+		
 	}
 }
