c0efd255b06c0ec27e4b7b8a3413ab05746290be
Kei <Kei@Kei-PC>
2018-06-01 02:06:37 -0300

    fix message

--- src/main/java/mail/MailMessage.java
+++ src/main/java/mail/MailMessage.java
@@ -1,15 +1,6 @@
 package mail;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import baterias_test.BateriaDeTest;
 import modelo.Menu;
-import modelo.MenuCreator;
-import modelo.Plato;
 
 public class MailMessage {
 	
